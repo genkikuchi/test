9cfc0d8e60ded3a801077670e8e86cb42113a0b4
Gen Kikuchi <kiku@nobworks.co.jp>
2013-12-14 15:34:04 +0900

    x

--- src/nobworks/proto1/client/Proto1.java
+++ src/nobworks/proto1/client/Proto1.java
@@ -17,26 +17,13 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class Proto1 implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
 	private static final String SERVER_ERROR = "An error occurred while "
 	    + "attempting to contact the server. Please check your network "
 	    + "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
 	private final GreetingServiceAsync greetingService = GWT.create( GreetingService.class );
 
-	/**
-	 * This is the entry point method.
-	 */
 	public void onModuleLoad() {
 		final Button sendButton = new Button( "Send" );
 		final TextBox nameField = new TextBox();
