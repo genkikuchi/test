6e98253c942fcb9486153305d3c2f23c5b0dab37
Gen Kikuchi <kiku@nobworks.co.jp>
2013-12-14 15:31:31 +0900

    2nd

--- src/nobworks/proto1/client/Proto1.java
+++ src/nobworks/proto1/client/Proto1.java
@@ -40,7 +40,7 @@ public class Proto1 implements EntryPoint {
 	public void onModuleLoad() {
 		final Button sendButton = new Button( "Send" );
 		final TextBox nameField = new TextBox();
-		nameField.setText( "GWT User" );
+		nameField.setText( "テスト" );
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
